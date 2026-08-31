23b2337ec73b7f591be2fd5661aeb4c956463459
Felipe Jonathan <felipejonathan.dev@gmail.com>
2026-08-31 19:18:45 -0300

    feat: adiciona entidade Task e interface ITaskRepository na camada de domínio

---
+++ Lagom.Domain/Tasks/ITaskRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Lagom.Domain.Tasks;
+
+public interface ITaskRepository
+{
+    public IEnumerable<Task> ListAll(DateOnly? startDate, DateOnly? endDate);
+    public Task? GetById(int id);
+    public Task RegisterTask(Task task);
+}
---
+++ Lagom.Domain/Tasks/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Lagom.Domain.Tasks;
+
+public class Task
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+    public int DurationInMinutes { get; set; } = 0;
+
+    public bool IsCompleted { get; set; } = false;
+}
