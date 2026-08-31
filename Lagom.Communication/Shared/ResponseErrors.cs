61711f77400a1d308d94aa8474a17903d97f5996
Felipe Jonathan <felipejonathan.dev@gmail.com>
2026-08-30 22:47:37 -0300

    feat: adiciona ResponseErrors para tratamento de erros da API

---
+++ Lagom.Communication/Shared/ResponseErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace Lagom.Communication.Shared;
+
+// TODO: Implementar o tratamento de erros que preenche e retorna esse objeto na API.
+public class ResponseErrors
+{
+    public List<string> Errors { get; set; } = [];
+}
